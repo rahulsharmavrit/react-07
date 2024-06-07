cd1daa9430bca277d8792fbbd3131d788e5d91a9
Rahul <rahul@vrittechnologies.com>
2024-06-07 12:57:59 +0545

    use reducer for counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
-import React from 'react';
-import mainImage from "./assets/images/abc.png";
-
+import React, { useReducer } from "react";
 
 const App = () => {
-  return (<>  
-  <img src="src\assets\images\abc.png" alt="" />
-  <img src="../src/assets/images/abc.png" alt="" />
-  <img src={mainImage} alt="" />
+  let initialState = 0;
 
-  </>)
-}
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "inc":
+        return (state = state + 1);
+      case "dec":
+        return (state = state - 1);
 
-export default App
+      default:
+        break;
+    }
+  };
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <>
+      <div className="" style={{ display: "flex", margin: 50 }}>
+        <button onClick={() => { dispatch({ type: "inc" }); }} > + </button>
+        <div className="" style={{ width: 50, textAlign: "center" }}>
+          {state}
+        </div>
+        <button onClick={() => { dispatch({ type: "dec" }); }} > - </button>
+
+      </div>
+    </>
+  );
+};
+
+export default App;
